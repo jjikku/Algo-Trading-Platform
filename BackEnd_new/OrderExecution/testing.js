e166276d7c6fe507113fdb5496fc5d7675037ed4
AneeshVDas <aneeshvdas@gmail.com>
2022-07-30 16:13:12 +0530

    Trade Execution checked in
    Trade Execution procedure checked in

---
+++ BackEnd_new/OrderExecution/testing.js
@@ -0,0 +1,6 @@
+const TRADE_EXECUTION = require('./trade_execution');
+instrumentId = "13793420" //ACC-EQ";
+orderquantity = 25 //1
+TRADE_EXECUTION.executetrade(instrumentId, orderquantity);
+//TRADE_EXECUTION.getorderdetails("", "");
+//TRADE_EXECUTION.getTradeDetails("", "");

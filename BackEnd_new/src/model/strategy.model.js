abfd2cfbd3165e81ca13a3545843c145757c9ffd
AneeshVDas <aneeshvdas@gmail.com>
2022-07-31 15:57:09 +0530

    changes done by minnu
    changes done by minnu

--- BackEnd_new/src/model/strategy.model.js
+++ BackEnd_new/src/model/strategy.model.js
@@ -1,19 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose=require('mongoose');
 mongoose.connect(
-  "mongodb+srv://jikku:jikku123@cluster0.ly4pn.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
+    "mongodb+srv://jikku:jikku123@cluster0.ly4pn.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useUnifiedTopology:true,
+        useNewUrlParser:true,
+    }
 );
 const Schema = mongoose.Schema;
+var StrategySchema=new Schema({
+ stratname: String,
+ strategy:String
 
-const StrategySchema = new Schema({
-  title: String,
-  strategy: String,
-  //creationDate: { type: DATE, default: Date.now }
-});
-
-const STRATEGY = mongoose.model("strategy", StrategySchema, "strategy");
-
-module.exports = STRATEGY;
+})
+var strategyModel=mongoose.model('strategy',StrategySchema,"strategy");
+module.exports=strategyModel;

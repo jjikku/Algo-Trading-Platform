d2976885443242bae44bd54647b3d19196581d40
AneeshVDas <aneeshvdas@gmail.com>
2022-08-07 21:42:45 +0530

    mutiple trade execution changes done
    mutiple trade execution changes done

--- BackEnd_new/src/oep/trade_execution.js
+++ BackEnd_new/src/oep/trade_execution.js
@@ -1,101 +1,82 @@
-var XTSInteractive = require('xts-interactive-api').Interactive;
-var XTSInteractiveWS = require('xts-interactive-api').WS;
-var config = require('./config.json');
+async function getInteractiveAPI() {
+    var XtsInteractiveAPI   = require('xts-interactive-api').Interactive;
+    var config              = require('./config.json');
+    let url                 = config.url;
+    var xtsInteractiveAPI   = new XtsInteractiveAPI(url);
+    return xtsInteractiveAPI;
+}
 
-let secretKey = config.secretKey;
-let appKey = config.appKey;
-let source = config.source;
-let url = config.url;
-let userID = null;
-
-//xtsInteractive for API calls and xtsInteractiveWS for events related functionality
-var xtsInteractive = null;
-var xtsInteractiveWS = null;
-
-async function executetrade(instrumentID, quantity, buy_sell) {
-        //creating the instance of XTSRest
-        xtsInteractive = new XTSInteractive(url);
-        //calling the logIn API
-        var loginRequest = {secretKey,appKey,source,};
-        let logIn = await xtsInteractive.logIn(loginRequest);
-        // checking for valid loginRequest
-        if (logIn && logIn.type == xtsInteractive.responseTypes.success) {
-            //creating the instance of XTSInteractiveWS
-            xtsInteractiveWS = new XTSInteractiveWS(url);
-            userID = logIn.result.userID;
-            //Instantiating the socket instance
-            // Token Generated after successful LogIn
-            var socketInitRequest = {userID: logIn.result.userID,token: logIn.result.token,};
-            xtsInteractiveWS.init(socketInitRequest);
-            //calling the remaining methods of the Interactive API
-            let executedorder = executeTradeAPI(instrumentID,quantity, buy_sell);
-            return executedorder;
-
-        } else {
-            //In case of failure
-            console.error(logIn);
-        }
+async function InteractiveAPILogin(xtsInteractiveAPI){
+    var config          = require('./config.json');
+    let secretKey       = config.secretKey;
+    let appKey          = config.appKey;
+    let source          = config.source;
+    var loginRequest    = {secretKey,appKey,source,};
+    let logIn           = await xtsInteractiveAPI.logIn(loginRequest);
+    if (logIn && logIn.type == xtsInteractiveAPI.responseTypes.success) {
+        userID = logIn.result.userID;
+        return userID;
+    } else {
+        console.error(logIn);
     }
-    async function executeTradeAPI(instrumentID,quantity, buy_sell) 
-    {
-        let placeOrderRequest = {
-        exchangeSegment: 'NSECM', //''NSECM
-        exchangeInstrumentID: instrumentID, //"SBIN-EQ", //22, BANKNIFTY 28JUL29SEP SPD
-        productType: 'MIS', //THIS IS FOR INTRADAY
-        orderType: 'MARKET',
-        orderSide: buy_sell,
+}   
 
-        timeInForce: 'DAY',
-        disclosedQuantity: 0,
-        orderQuantity: quantity,
-        limitPrice: 2000,
-        stopPrice: 0,
-        orderUniqueIdentifier: '45485',
-        clientID: userID
-        };
+async function executeTradeAPI(instrumentID,quantity, buy_sell, xtsInteractive) 
+{
+    let placeOrderRequest = {
+        exchangeSegment:        'NSEFO',
+        exchangeInstrumentID:   instrumentID,
+        productType:            'MIS', 
+        orderType:              'MARKET',
+        orderSide:              buy_sell,
+        timeInForce:            'DAY',
+        disclosedQuantity:      0,
+        orderQuantity:          quantity,
+        limitPrice:             2000,
+        stopPrice:              0,
+        orderUniqueIdentifier:  '45485',
+        clientID:               userID
+    };
 
-        let response =await placeOrder(placeOrderRequest);
-        return response; 
-    }
+    let tradeprice      = 0;
+    let responseorder   = await xtsInteractive.placeOrder(placeOrderRequest);
+    var orderype        =  responseorder.type;
 
-    var placeOrder = async function (placeOrderRequest) {
-        let tradeprice = 0;
-        let responseorder = await xtsInteractive.placeOrder(placeOrderRequest);
-        var orderype =  responseorder.type;
-
-        if(orderype=="success"){
-            tradeprice = await getOrderAPI(responseorder.result.AppOrderID);
-        }
-        else{
-            tradeprice =  ({
-                message: 'error occured!',
-                executionPrice:0,
-                apporderNo:'',
-                data:responseorder
-            });
-        }
-        return tradeprice;
-    }
-
-    async function getOrderAPI(appOrderID) 
-    {
-        let response = await xtsInteractive.getOrderBook();
-        var tradetype =  response.type;
+    if(orderype=="success"){
+        
+        let appOrderID  = responseorder.result.AppOrderID
+        let response    = await xtsInteractive.getOrderBook();
+        var tradetype   =  response.type;
         var tradeexecutedprice = 0;
 
         if(tradetype=="success"){
             var orderdetailsfind = response.result;
-            for(i=0;i<orderdetailsfind.length;i++)
+            for(let pricei=0; pricei<orderdetailsfind.length; pricei++)
             {
-                if(appOrderID == orderdetailsfind[i].AppOrderID){
-                    tradeexecutedprice = orderdetailsfind[i].OrderAverageTradedPrice;
+                if(appOrderID == orderdetailsfind[pricei].AppOrderID){
+                    tradeexecutedprice = orderdetailsfind[pricei].OrderAverageTradedPrice;
                 }
             }
         }
-        return ({
-            message: 'Order placed sucessfully!',
-            executionPrice:tradeexecutedprice,
-            apporderNo:appOrderID
+        tradeprice = ({
+            message:                'Order placed sucessfully!',
+            tradeprice:             tradeexecutedprice,
+            apporderNo:             appOrderID,
+            data:                   responseorder,
+            exchangeInstrumentID:   placeOrderRequest.exchangeInstrumentID,
+            buy_sell:               placeOrderRequest.orderSide
+        })
+    }
+    else{
+        tradeprice =  ({
+            message:                'error occured!',
+            executionPrice:         0,
+            apporderNo:             '',
+            data:                   responseorder,
+            exchangeInstrumentID:   placeOrderRequest.exchangeInstrumentID,
+            buy_sell:               placeOrderRequest.orderSide
         });
     }
-module.exports = {executetrade}
+    return tradeprice;
+}
+module.exports = {executeTradeAPI, getInteractiveAPI, InteractiveAPILogin}

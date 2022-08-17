aa3b5852e4b7643d5c8ab93f9036a0557b01a51c
soorejvs <vs.soorej@gmail.com>
2022-08-17 21:34:23 +0400

    CSS Edit User
    CSS Edit User

--- FrontEnd_new/src/services/edituser.service.ts
+++ FrontEnd_new/src/services/edituser.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient ,HttpResponse} from '@angular/common/http'
+import { CommonURL } from 'src/services/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class EdituserService {
   editUser(id:any, user:any)
   {   
     console.log("Edituser Service editUser Fun=  "+id,user);
-     return this.http.post("http://localhost:5000/edituser/"+id,{"user":user});
+    return this.http.post(`${CommonURL.BASE_URL}/edituser/`+id,{"user":user});
+
+    // return this.http.post("http://localhost:5000/edituser/"+id,{"user":user});
     //return this.http.post<any>("http://localhost:5000/edituser/"+id,{"user":item});
   }
 }
